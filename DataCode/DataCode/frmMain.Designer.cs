a751f72ece8e7847a187881cc8cd5d8615dcb51f
guochao2299 <szf.0325@gmail.com>
2017-03-16 22:46:21 +0800

    Add files via upload

---
+++ DataCode/DataCode/frmMain.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace DataCode
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtResultB = new System.Windows.Forms.TextBox();
+            this.txtResultF = new System.Windows.Forms.TextBox();
+            this.txtResultY = new System.Windows.Forms.TextBox();
+            this.txt2stB = new System.Windows.Forms.TextBox();
+            this.txt2stF = new System.Windows.Forms.TextBox();
+            this.txt2stY = new System.Windows.Forms.TextBox();
+            this.txt1stB = new System.Windows.Forms.TextBox();
+            this.txt1stF = new System.Windows.Forms.TextBox();
+            this.txt1stY = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nud2st = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nud1st = new System.Windows.Forms.NumericUpDown();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2st)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1st)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.57895F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.57895F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.84211F));
+            this.tableLayoutPanel1.Controls.Add(this.txtResultB, 5, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtResultF, 5, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtResultY, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txt2stB, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txt2stF, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txt2stY, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txt1stB, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txt1stF, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txt1stY, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.nud2st, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.nud1st, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtResult, 5, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 127);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtResultB
+            // 
+            this.txtResultB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResultB.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtResultB.Location = new System.Drawing.Point(415, 93);
+            this.txtResultB.Name = "txtResultB";
+            this.txtResultB.ReadOnly = true;
+            this.txtResultB.Size = new System.Drawing.Size(182, 29);
+            this.txtResultB.TabIndex = 17;
+            // 
+            // txtResultF
+            // 
+            this.txtResultF.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResultF.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtResultF.Location = new System.Drawing.Point(415, 63);
+            this.txtResultF.Name = "txtResultF";
+            this.txtResultF.ReadOnly = true;
+            this.txtResultF.Size = new System.Drawing.Size(182, 29);
+            this.txtResultF.TabIndex = 16;
+            // 
+            // txtResultY
+            // 
+            this.txtResultY.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResultY.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtResultY.Location = new System.Drawing.Point(415, 33);
+            this.txtResultY.Name = "txtResultY";
+            this.txtResultY.ReadOnly = true;
+            this.txtResultY.Size = new System.Drawing.Size(182, 29);
+            this.txtResultY.TabIndex = 15;
+            // 
+            // txt2stB
+            // 
+            this.txt2stB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt2stB.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt2stB.Location = new System.Drawing.Point(234, 93);
+            this.txt2stB.Name = "txt2stB";
+            this.txt2stB.ReadOnly = true;
+            this.txt2stB.Size = new System.Drawing.Size(155, 29);
+            this.txt2stB.TabIndex = 14;
+            // 
+            // txt2stF
+            // 
+            this.txt2stF.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt2stF.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt2stF.Location = new System.Drawing.Point(234, 63);
+            this.txt2stF.Name = "txt2stF";
+            this.txt2stF.ReadOnly = true;
+            this.txt2stF.Size = new System.Drawing.Size(155, 29);
+            this.txt2stF.TabIndex = 13;
+            // 
+            // txt2stY
+            // 
+            this.txt2stY.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt2stY.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt2stY.Location = new System.Drawing.Point(234, 33);
+            this.txt2stY.Name = "txt2stY";
+            this.txt2stY.ReadOnly = true;
+            this.txt2stY.Size = new System.Drawing.Size(155, 29);
+            this.txt2stY.TabIndex = 12;
+            // 
+            // txt1stB
+            // 
+            this.txt1stB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt1stB.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt1stB.Location = new System.Drawing.Point(53, 93);
+            this.txt1stB.Name = "txt1stB";
+            this.txt1stB.ReadOnly = true;
+            this.txt1stB.Size = new System.Drawing.Size(155, 29);
+            this.txt1stB.TabIndex = 11;
+            // 
+            // txt1stF
+            // 
+            this.txt1stF.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt1stF.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt1stF.Location = new System.Drawing.Point(53, 63);
+            this.txt1stF.Name = "txt1stF";
+            this.txt1stF.ReadOnly = true;
+            this.txt1stF.Size = new System.Drawing.Size(155, 29);
+            this.txt1stF.TabIndex = 10;
+            // 
+            // txt1stY
+            // 
+            this.txt1stY.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt1stY.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt1stY.Location = new System.Drawing.Point(53, 33);
+            this.txt1stY.Name = "txt1stY";
+            this.txt1stY.ReadOnly = true;
+            this.txt1stY.Size = new System.Drawing.Size(155, 29);
+            this.txt1stY.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 30);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "补码";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 30);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "反码";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "原码";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(395, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "=";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nud2st
+            // 
+            this.nud2st.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nud2st.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nud2st.Location = new System.Drawing.Point(234, 3);
+            this.nud2st.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.nud2st.Minimum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            -2147483648});
+            this.nud2st.Name = "nud2st";
+            this.nud2st.Size = new System.Drawing.Size(155, 29);
+            this.nud2st.TabIndex = 3;
+            this.nud2st.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nud2st.ValueChanged += new System.EventHandler(this.nud2st_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(214, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "+";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nud1st
+            // 
+            this.nud1st.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nud1st.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nud1st.Location = new System.Drawing.Point(53, 3);
+            this.nud1st.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.nud1st.Minimum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            -2147483648});
+            this.nud1st.Name = "nud1st";
+            this.nud1st.Size = new System.Drawing.Size(155, 29);
+            this.nud1st.TabIndex = 1;
+            this.nud1st.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nud1st.ValueChanged += new System.EventHandler(this.nud1st_ValueChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Font = new System.Drawing.Font("SimSun", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtResult.Location = new System.Drawing.Point(415, 3);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(182, 29);
+            this.txtResult.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 127);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "原码、反码和补码";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2st)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1st)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nud1st;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nud2st;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtResultB;
+        private System.Windows.Forms.TextBox txtResultF;
+        private System.Windows.Forms.TextBox txtResultY;
+        private System.Windows.Forms.TextBox txt2stB;
+        private System.Windows.Forms.TextBox txt2stF;
+        private System.Windows.Forms.TextBox txt2stY;
+        private System.Windows.Forms.TextBox txt1stB;
+        private System.Windows.Forms.TextBox txt1stF;
+        private System.Windows.Forms.TextBox txt1stY;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DataCode/DataCode/frmMain.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataCode
+{
+    // now only support calculate integer 
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            UpdateFirstValueCode();
+            UpdateSecondValueCode();
+        }
+
+        private void UpdateFirstValueCode()
+        {
+            txt1stY.Text = CalculateTrueForm(Convert.ToInt32(nud1st.Value));
+            txt1stF.Text = CalculateRadixMinusOneComplement(txt1stY.Text);
+            txt1stB.Text = CalculateComplement(txt1stF.Text);
+        }
+
+        private void UpdateSecondValueCode()
+        {
+            txt2stY.Text = CalculateTrueForm(Convert.ToInt32(nud2st.Value));
+            txt2stF.Text = CalculateRadixMinusOneComplement(txt2stY.Text);
+            txt2stB.Text = CalculateComplement(txt2stF.Text);
+        }
+
+        private void nud1st_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateFirstValueCode();
+            UpdateResult();
+        }
+
+        private void nud2st_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateSecondValueCode();
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            txtResult.Text = Convert.ToString(Convert.ToInt32(nud1st.Value) + Convert.ToInt32(nud2st.Value));
+            txtResultY.Text = CalculateCodeResult(txt1stY.Text, txt2stY.Text);
+            txtResultF.Text = CalculateCodeResult(txt1stF.Text, txt2stF.Text);
+            txtResultB.Text = CalculateCodeResult(txt1stB.Text, txt2stB.Text);
+        }
+
+        private class CalculateCathe
+        {
+            public bool IsCarry = false;
+            public char Result;
+        }
+
+        private CalculateCathe CalculateBitResult(char c1, char c2, bool carry)
+        {
+            CalculateCathe cathe = new CalculateCathe();
+
+            int intResult=c1-'0'+c2-'0'+(carry?1:0);
+            cathe.IsCarry=intResult>1;
+            cathe.Result=Convert.ToChar((intResult%2)+'0');
+            
+            return cathe;
+        }
+
+        private bool IsCarry(char c1, char c2)
+        {
+            return (c1 == SECONT_CHAR_B) &&
+                (c2 == SECONT_CHAR_B);
+        }
+
+        public string CalculateCodeResult(string data1st,string data2st)
+        {
+            StringBuilder result = new StringBuilder();
+            CalculateCathe buffer = new CalculateCathe();
+
+            for (int i = data1st.Length - 1; i >= 0; i--)
+            {
+                buffer = CalculateBitResult(data1st[i], data2st[i], buffer.IsCarry);                
+                result.Insert(0, buffer.Result);                
+            }
+
+            return result.ToString();
+        }
+
+        private string CalculateTrueForm(int originalValue)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            int quotient = 0;
+            int remainder = 0;
+
+            int tmp=Math.Abs(originalValue);
+
+            do
+            {
+                quotient = tmp / 2;
+                remainder = tmp % 2;
+
+                buffer.Insert(0,Convert.ToString(remainder));
+
+                tmp = quotient;
+            } while (tmp != 0);
+
+            string result = buffer.ToString().TrimStart(FIRST_CHAR_B).PadLeft(7, FIRST_CHAR_B);
+
+            return (Convert.ToString(originalValue < 0 ? SECONT_CHAR_B : FIRST_CHAR_B)) + result;
+        }
+
+        private const char POSITIVE_SIGN = '0';
+        private const char NEGATIVE_SIGN = '1';
+        private const char FIRST_CHAR_B = '0';
+        private const char SECONT_CHAR_B = '1';
+
+        public string CalculateComplement(string dataF)
+        {
+            if (dataF[0] == POSITIVE_SIGN)
+            {
+                return dataF;
+            }
+
+            StringBuilder result = new StringBuilder();
+            
+            bool carry = dataF.Last() == SECONT_CHAR_B;
+            result.Append(carry ? FIRST_CHAR_B : SECONT_CHAR_B);
+
+            for (int i = dataF.Length - 2; i >= 0; i--)
+            {
+                if (carry)
+                {
+                    carry = dataF[i] == SECONT_CHAR_B;
+                    result.Insert(0,carry ? FIRST_CHAR_B : SECONT_CHAR_B);
+
+                    continue;
+                }
+
+                result.Insert(0, dataF[i]);
+            }
+
+            return result.ToString();
+        }
+
+        public string CalculateRadixMinusOneComplement(string dataY)
+        {
+            if (dataY[0] == POSITIVE_SIGN)
+            {
+                return dataY;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(dataY[0]);
+
+            for (int i = 1; i < dataY.Length; i++)
+            {
+                sb.Append(dataY[i] == FIRST_CHAR_B ? SECONT_CHAR_B : FIRST_CHAR_B);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
